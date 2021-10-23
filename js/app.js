879477a2743e28f82bb3349802513c0c5e28f3bc
nahidthenh <nhgroup18@gmail.com>
2021-10-24 01:59:30 +0600

    header

--- js/app.js
+++ js/app.js
@@ -1,3 +1,69 @@
+// DropDown Trogol
+
+$(".bdkaj-dropdown-toggle").on("click", function () {
+    if ($(this).closest(".bdkaj-dropdown").find(".bdkaj-dropdown-list").hasClass("bdkaj--open")) {
+        $(this).find('.arrow-icon').animate({
+            'rotate': '0'
+        }, {
+            step: function (now, fx) {
+                $(this).css({
+                    "transform": "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(-" +
+                        now +
+                        "deg) skew(0deg, 0deg)",
+                    "transform-style": "preserve-3d"
+                });
+            },
+            duration: 180,
+            easing: 'linear',
+            queue: false,
+            complete: function () {
+
+            }
+        }, 'linear');
+        $(this).closest(".bdkaj-dropdown").find(".bdkaj-dropdown-list").removeClass('bdkaj--open');
+    } else {
+        $('.arrow-icon').animate({
+            'rotate': '0'
+        }, {
+            step: function (now, fx) {
+                $(this).css({
+                    "transform": "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(-" +
+                        now +
+                        "deg) skew(0deg, 0deg)",
+                    "transform-style": "preserve-3d"
+                });
+            },
+            duration: 80,
+            easing: 'linear',
+            queue: false,
+            complete: function () {
+
+            }
+        }, 'linear');
+        $(".bdkaj-dropdown-list").removeClass('bdkaj--open');
+
+        $(this).find('.arrow-icon').animate({
+            'rotate': '180'
+        }, {
+            step: function (now, fx) {
+                $(this).css({
+                    "transform": "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(-" +
+                        now +
+                        "deg) skew(0deg, 0deg)",
+                    "transform-style": "preserve-3d"
+                });
+            },
+            duration: 180,
+            easing: 'linear',
+            queue: false,
+            complete: function () {
+
+            }
+        }, 'linear');
+        $(this).closest(".bdkaj-dropdown").find(".bdkaj-dropdown-list").addClass('bdkaj--open');
+    }
+});
+
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 15,
@@ -20,3 +86,9 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+
+
+
+
